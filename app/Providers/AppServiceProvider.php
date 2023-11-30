8bf0aaed116dd089b9ead486f71fd601cbd29a74
alainware <irving6258@gmail.com>
2023-11-30 14:49:14 -0600

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +17,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $urlGenerator): void
     {
-        //
+        if(env('APP_ENV') !== 'production')
+        {
+            $url->forceSchema('https');
+        }
     }
 }
